8ce7f31c000a2aebc7adc2c28a949d8bf88ab602
Sandeep <dandasandeep098@gmail.com>
2026-07-22 21:53:01 +0530

    Updated backend url

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,5 +1,5 @@
 // const BASE_URL="http://localhost:8080/api"
-const BASE_URL="https://cloudshare-backend-production-32b6.up.railway.app"
+const BASE_URL="https://cloudshare-backend-production-32b6.up.railway.app/api"
 
 export const apiEndpoints = {
     FETCH_FILES:`${BASE_URL}/files/my`,
